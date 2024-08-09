4aa322241d455a4f4cb3f03c77b1e390fb311144
Herbert Tamayo <hftamayo@gmail.com>
2024-08-09 15:42:14 -0600

    stub object for pdf generator

--- pkg/pdf/pdf_generator.go
+++ pkg/pdf/pdf_generator.go
@@ -1,9 +1,28 @@
 package pdf
 
 import (
+	"log"
+
 	"github.com/jung-kurt/gofpdf"
 )
 
 type PDFGenerator struct {
 	pdf *gofpdf.Fpdf
+}
+
+func NewPDFGenerator() *PDFGenerator {
+	pdf := gofpdf.New("P", "mm", "A4", "")
+	return &PDFGenerator{pdf: pdf}
+}
+
+func (pg *PDFGenerator) GenerateSamplePDF(filename string) error {
+	pg.pdf.AddPage()
+	pg.pdf.SetFont("Arial", "B", 16)
+	pg.pdf.Cell(40, 10, "Hello, World!")
+	err := pg.pdf.OutputFileAndClose(filename)
+	if err != nil {
+		log.Printf("Error generating PDF: %v", err)
+		return err
+	}
+	return nil
 }
