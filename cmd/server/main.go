99332eae2668d8abfdcd1fd3c7fe9a053bde5614
Herbert Tamayo <hftamayo@gmail.com>
2024-08-15 15:45:09 -0600

    updates on main function

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,14 @@
 package main
 
 import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
 	"github.com/gin-gonic/gin"
 	"github.com/hftamayo/gorptmservice/internal/handlers"
 )
@@ -10,5 +18,40 @@ func main() {
 
 	router.POST("/generate-report", handlers.GenerateReportHandler)
 
-	router.Run(":8004") // Listen and serve on 0.0.0.0:8080
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8004"
+	}
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: router,
+	}
+
+	// Create a channel to listen for termination signals
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Failed to run server: %v", err)
+		}
+	}()
+
+	log.Printf("Server is running on port %s", port)
+
+	// Block until a termination signal is received
+	<-quit
+	log.Println("Shutting down server...")
+
+	// Create a context with a timeout of 5 seconds
+	// This will allow the server to gracefully shutdown
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("Server forced to shutdown: %v", err)
+	}
+
+	log.Println("Server exiting")
 }
