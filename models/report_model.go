cd5705ac04cc098c1917b5ce4506f5d92e643da0
Herbert Tamayo <hftamayo@gmail.com>
2024-08-15 15:31:08 -0600

    last part of report model file

--- models/report_model.go
+++ models/report_model.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	"encoding/json"
+	"errors"
+)
+
 type Report struct {
 	Title     string `json:"title"`
 	Author    string `json:"author"`
@@ -14,4 +19,28 @@ type ReportContent struct {
 type ReportSection struct {
 	Title   string `json:"title"`
 	Content string `json:"content"`
+}
+
+func (r *Report) Validate() error {
+	if r.Title == "" {
+		return errors.New("title is required")
+	}
+
+	if r.Author == "" {
+		return errors.New("author is required")
+	}
+
+	if r.Content == "" {
+		return errors.New("content is required")
+	}
+
+	return nil
+}
+
+func (r *Report) ToJSON() ([]byte, error) {
+	return json.Marshal(r)
+}
+
+func (r *Report) FromJSON(data []byte) error {
+	return json.Unmarshal(data, r)
 }
